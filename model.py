62159b6bc83b0cea4ce445cb070c2b9721e7294a
SaewonY <chrisyang9305@gmail.com>
2019-12-28 14:55:07 +0900

    add py files

---
+++ model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+from torch import nn, cuda
+from torch.nn import functional as F
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+class NeuralNet(nn.Module):
+
+    def __init__(self, embedding_matrix):
+        super(NeuralNet, self).__init__()
+
+        lstm_hidden_size = 120
+        gru_hidden_size = 60
+        self.gru_hidden_size = gru_hidden_size
+
+        self.embedding = nn.Embedding(*embedding_matrix.shape)
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+        self.embedding_dropout = nn.Dropout2d(0.2)
+
+        self.lstm = nn.LSTM(embedding_matrix.shape[1], lstm_hidden_size, bidirectional=True, batch_first=True)
+        self.gru = nn.GRU(lstm_hidden_size * 2, gru_hidden_size, bidirectional=True, batch_first=True)
+
+        self.linear = nn.Linear(gru_hidden_size * 6, 20)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.1)
+        self.out = nn.Linear(20, 1)
+        
+    def apply_spatial_dropout(self, h_embedding):
+        h_embedding = h_embedding.transpose(1, 2).unsqueeze(2)
+        h_embedding = self.embedding_dropout(h_embedding).squeeze(2).transpose(1, 2)
+        return h_embedding
+
+    def forward(self, x, normal_feats, lengths=None):
+        h_embedding = self.embedding(x.long())
+        h_embedding = self.apply_spatial_dropout(h_embedding)
+
+        h_lstm, _ = self.lstm(h_embedding)
+        h_gru, hh_gru = self.gru(h_lstm)
+
+        hh_gru = hh_gru.view(-1, self.gru_hidden_size * 2)
+
+        avg_pool = torch.mean(h_gru, 1)
+        max_pool, _ = torch.max(h_gru, 1)
+
+#         normal_linear  = F.relu(self.normal_linear(normal_feats.float()))
+
+        conc = torch.cat((hh_gru, avg_pool, max_pool), 1)
+        conc = self.relu(self.linear(conc))
+        conc = self.dropout(conc)
+        out = self.out(conc)
+
+        return out
